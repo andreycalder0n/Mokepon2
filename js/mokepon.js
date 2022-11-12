a1fbbc92f05797da9a064997b2fa02694765d8bc
Andrey Calderon <eltercercafe@gmail.com>
2022-11-12 16:11:59 -0500

    Add JS

---
+++ js/mokepon.js
@@ -0,0 +1,6 @@
+let playerMokeponButton = document.getElementById('player-mokepon-button')
+playerMokeponButton.addEventListener('click', selectPlayerMokepon)
+
+function selectPlayerMokepon() {
+    alert('Selected your pet')
+}
